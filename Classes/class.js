d70aa56c38c3f963666efbb96261d9138c8a9e8f
Israel Manzo <israelmanzo814@gmail.com>
2018-04-13 13:06:52 -0400

    Properties
    Adding some properties to the class

--- Classes/class.js
+++ Classes/class.js
@@ -1,8 +1,17 @@
  // Initialized class
 class Human {
-
+    // properties
+    constructor(){
+        this.arms = 2;
+        this.head = 1;
+        this.myName = name;
+    }
 }
 
 // Instances of a class
 
-const species = new Human;
+const species = new Human('Israel');
+
+console.log('The human has ' + species.arms + ' arms');
+
+console.log('The human is ' + species.myName);
