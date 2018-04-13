da41c49714ebb98a6706f85119ea1cab30576b59
Israel Manzo <israelmanzo814@gmail.com>
2018-04-13 13:11:11 -0400

    Instance of the class
    Creating instance of the class

--- Classes/class.js
+++ Classes/class.js
@@ -1,17 +1,20 @@
  // Initialized class
 class Human {
     // properties
-    constructor(name){
+    constructor(name, age){
         this.arms = 2;
         this.head = 1;
         this.myName = name;
+        this.myAge = age;
     }
 }
 
 // Instances of a class
 
-const species = new Human('Israel');
+const species = new Human('Israel', 42);
 
 console.log('The human has ' + species.arms + ' arms');
 
 console.log('The human is ' + species.myName);
+
+console.log(species.myName + ' is ' + species.myAge + ' years old!');
