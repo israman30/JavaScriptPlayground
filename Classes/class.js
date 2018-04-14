05c7f2adf078ddcd92188e6337ee364c60fe25da
Israel Manzo <israelmanzo814@gmail.com>
2018-04-13 20:23:59 -0400

    method added to the class
    Added a  method to the class

--- Classes/class.js
+++ Classes/class.js
@@ -7,16 +7,21 @@ class Human {
         this.myName = name;
         this.myAge = age;
     }
+
+    walk() {
+        return `${this.myName} is ${this.myAge} years old and walks...!`;
+    }
 }
 
 // Instances of a class
 
-const species = new Human('Israel', 42);
-const person = new Human('Audra', 40);
+const person1 = new Human('Israel', 42);
+const person2 = new Human('Audra', 40);
 
-console.log('The human has ' + species.arms + ' arms');
-console.log(person.myName + ' is my wife, she is ' + person.myAge + ' years old!');
+// console.log('The human has ' + species.arms + ' arms');
+// console.log(person.myName + ' is my wife, she is ' + person.myAge + ' years old!');
 
-console.log('The human is ' + species.myName);
+// console.log('The human is ' + species.myName);
 
-console.log(species.myName + ' is ' + species.myAge + ' years old!');
+// console.log(species.myName + ' is ' + species.myAge + ' years old!');
+console.log(person1.walk());
