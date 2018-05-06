deec72c90d5fd65fb1a283d5d910c137c15dbddc
Israel Manzo <israelmanzo814@gmail.com>
2018-05-05 23:02:02 -0400

    Added some more fu stuff

--- Date/date.js
+++ Date/date.js
@@ -2,3 +2,7 @@
 function showDate(){
     document.getElementById('show').innerHTML = "Show raw date here: " + Date();
 }
+
+function changeP(){
+    document.getElementById('change').innerHTML = "JavaScript is awesome!!!";
+}
