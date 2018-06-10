420fa477abb5f1e5d72b2b08b1947831f221f526
Israel Manzo <israelmanzo814@gmail.com>
2018-06-09 21:35:48 -0400

    Updating object constructor file js

---
+++ OOP/constructor.js
@@ -0,0 +1,12 @@
+// constructor
+function Book(title, author, year){
+    this.title = title;
+    this.author = author;
+    this.year = year;
+}
+
+// Initialize
+const book3 = new Book('Jungle Book', 'Someone', '2016');
+const book4 = new Book('Peter Pan', 'Another person', '2000');
+
+console.log(book3);
