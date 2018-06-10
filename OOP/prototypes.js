6f017fe09c0aa93a22956e7a449eaf5cc35512a0
Israel Manzo <israelmanzo814@gmail.com>
2018-06-09 21:44:38 -0400

    Added prototype age

--- OOP/prototypes.js
+++ OOP/prototypes.js
@@ -12,8 +12,15 @@ Book.prototype.getInfo = function() {
     ${this.author} in ${this.year}`;
 }
 
+// getAge
+Book.prototype.getAge = function(){
+    const years = new Date().getFullYear() - this.year;
+    return `${this.title} is ${years} years old.`
+}
+
 // Initialize
 const book5 = new Book('Jungle Book', 'Someone', '2016');
 const book6 = new Book('Peter Pan', 'Another person', '2000');
 
 console.log(book5.getInfo());
+console.log(book5.getAge());
